2a5249f71d81fc61a7d38fdeda361ed5a2c9eedd
tantowi.setiawan <tantowi.setiawan@binus.ac.id>
2024-05-10 22:42:38 +0700

    C++: Added Second Approach for Hourglass Algorithm

--- Shape Algorithm/hourglass.cpp
+++ Shape Algorithm/hourglass.cpp
@@ -19,9 +19,36 @@ void PrintHourGlass(int rowCount)
         cout << endl;
     } 
 }
+
+/* Second Approach */
+void SecondPrintHourGlass(int rowCount)
+{
+    for(int i = 0; i < rowCount * 2; i++)
+    {
+        if(i < (rowCount * 2) / 2) {
+            for(int j = 0; j < rowCount; j++) {
+                if(j < i) cout << " ";
+                else cout << "* ";
+            }
+        } else {
+            for(int j = 0 ; j < rowCount; j++) {
+                int tempDiff = ((rowCount - 1) - (i - rowCount));
+
+                if(j < tempDiff) cout << " ";
+                else cout << "* ";
+            }
+        }
+
+        cout << endl;
+    }
+
+}
+
 int main() {
     int number = 4;
     PrintHourGlass(number);
+    cout << "\n" << endl;
+    SecondPrintHourGlass(number);
 
     return 0;
 }
