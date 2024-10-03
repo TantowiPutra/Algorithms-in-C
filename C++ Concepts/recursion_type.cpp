e4971bfe8296480bfaaa909c12d39da3858f71a9
tantowi.setiawan <tantowi.setiawan@binus.ac.id>
2024-10-03 20:03:11 +0700

    Tree Recursion & Indirect Recursion

--- C++ Concepts/recursion_type.cpp
+++ C++ Concepts/recursion_type.cpp
@@ -11,8 +11,64 @@ void tailRecursion(int n)
     }
 }
 
+void headRecursion(int n)
+{
+    if(n > 0)
+    {
+        headRecursion(n - 1);
+        cout << n << endl;
+    }
+}
+
+void treeRecursion(int n)
+{
+    if(n > 0)
+    {
+        cout << n << endl;
+        treeRecursion(n - 1);
+        treeRecursion(n - 1);
+    }
+}
+
+// INDIRECT RECURSIOIN
+void funcB(int n);
+
+void funcA(int n)
+{
+    if(n > 0)
+    {
+        cout << n << endl;
+        funcB(n - 1);
+    }
+}
+
+void funcB(int n)
+{
+    if(n > 1)
+    {
+        cout << n << endl;
+        funcA(n / 2);
+    }
+}
+
+void funcHalf(int n)
+{
+    if(n > 0)
+    {
+        cout << n << endl;
+        funcHalf(n / 2);
+    }
+}
+
 int main() {
+    cout << "Tail Recursion: " << endl;
     tailRecursion(3);
     cout << "=============================" << endl;
+    cout << "Head Recursion: " << endl;
+    headRecursion(3);
+    cout << "Tree Recursion: " << endl;
+    treeRecursion(3);
+    cout << "Indirect Recursion: " << endl;
+    funcA(20);
     return 0;
 }
--- C++ Concepts/tempCodeRunnerFile.cpp
+++ C++ Concepts/tempCodeRunnerFile.cpp
@@ -1,3 +1,30 @@
+#include <iostream>
+using namespace std;
 
+void tailRecursion(int n) 
+{
+    // TAIL RECURSION IS A TYPE OF RECURSION WHERE THE FUNCTION CALL IS LOCATED AT THE LAST
+    if(n > 0)
+    {
+        cout << n << endl;
+        tailRecursion(n - 1);
+    }
+}
+
+void headRecursion(int n)
+{
+    if(n > 0)
+    {
+        headRecursion(n - 1);
+        cout << n << endl;
+    }
+}
+
+int main() {
+    cout << "Tail Recursion: " << endl;
+    tailRecursion(3);
+    cout << "=============================" << endl;
+    cout << "Head Recursion: " << endl;
+    headRecursion(3);
     return 0;
 }
