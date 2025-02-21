ffe6477277acd1eb25ef1ad55764c9677e4bf0e4
unknown <tantowi6758@gmail.com>
2025-02-21 08:54:44 +0700

    Recursion: Taylor's series

---
+++ C++ Concepts/Practice/taylorseries.cpp
@@ -0,0 +1,32 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+// TIME COMPLEXITY O(N)
+float TaylorSeries(int n, int x) {
+    static float p = 1, f =1;
+    
+    if(x == 0) {
+        return 1;
+    } else {
+        int r = TaylorSeries(n, x - 1);
+        p = p * n;
+        f = f * x;
+    
+        printf("P: %f\n", p);
+        printf("F: %f\n", f);
+        
+        return r + p / f;
+    }
+}
+
+int main() {
+    int n = 2;
+    int x = 4;
+    
+    cout << TaylorSeries(n, x) << endl;
+
+    return 0;
+}
