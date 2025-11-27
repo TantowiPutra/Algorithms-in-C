9b1240d4a36dd93c443e400db1e50406d484147b
unknown <tantowi6758@gmail.com>
2025-11-27 16:24:36 +0700

    Sorting Algorithm: Merge Sort (Optimized)

--- Sorting Algorithm/merge_sort.cpp
+++ Sorting Algorithm/merge_sort.cpp
@@ -1,38 +1,27 @@
-// Online C++ compiler to run C++ program online
 #include <bits/stdc++.h>
 using namespace std;
 
-void merge(vector<int> &arr, int l, int mid, int r) {
-    int n1 = mid - l + 1;
-    int n2 = r - mid;
-    
-    vector<int> v1, v2;
-    for(int i = 0; i < n1; i++) {
-        v1.push_back(arr[l + i]);
-    }
-    
-    for(int i = 0; i < n2; i++) {
-        v2.push_back(arr[mid + 1 + i]);
-    }
-    
-    // Insert Value ke Original Array
-    int i = 0, j = 0, k = l;
-    
-    while(i < n1 && j < n2) {
-        if(v1[i] <= v2[j]) {
-            arr[k++] = v1[i++];
+vector<int> temp;
+void merge(vector<int> &arr, int l, int mid, int r) {    
+    int i = l, j = mid + 1, k = l;
+
+    while(i <= mid && j <= r) {
+        if(arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
         } else {
-            arr[k++] = v2[j++];
+            temp[k++] = arr[j++];
         }
     }
     
-    while(i < n1) {
-        arr[k++] = v1[i++];
+    while(i <= mid) {
+        temp[k++] = arr[i++];
     }
     
-    while(j < n2) {
-        arr[k++] = v2[j++];
+    while(j <= r) {
+        temp[k++] = arr[j++];
     }
+
+    for(int i = l; i < k; i++) arr[i] = temp[i];
 }
 
 void mergeSort(vector<int> &arr, int l, int r) {
@@ -53,6 +42,7 @@ void printArr(vector<int> &arr) {
 
 int main() {
     vector<int> arr = {30, 20 , 10, 5, -10, 20, 5, 35};
+    temp.resize(arr.size());
     mergeSort(arr, 0, arr.size() - 1);
     printArr(arr);
     
