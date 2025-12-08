7a552d4b892259bc3d4a14ef9e32a685b8a83076
unknown <tantowi6758@gmail.com>
2025-12-08 16:49:25 +0700

    DS: Max/Min Heap - Insertion, Deletion

---
+++ Data Structures/Heap/max.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(vector<int> &maxHeap) {
+    for(int &a : maxHeap) cout << a << " ";
+    cout << endl;
+}
+
+// O(Log N) Heap Insertion Process
+void insert(vector<int> &maxHeap, int val) {
+    maxHeap.push_back(val);
+    
+    int currIdx   = maxHeap.size() - 1;
+    int parentIdx = (currIdx - 1) / 2; 
+
+    while(currIdx > 0 && val > maxHeap[parentIdx]) {
+        maxHeap[currIdx] = maxHeap[parentIdx];
+        
+        currIdx = parentIdx;
+        parentIdx = (currIdx - 1) / 2;
+    }
+    
+    maxHeap[currIdx] = val;
+}
+
+// O(N Log N) Heap Insertion Process
+void insertInPlace(vector<int> &maxHeap, int idx) {
+    int temp = maxHeap[idx];
+
+    while(idx > 0 && temp > maxHeap[(idx - 1) / 2]) {
+        maxHeap[idx] = maxHeap[(idx - 1) / 2];
+        idx          = (idx - 1) / 2;
+    }
+
+    maxHeap[idx] = temp;
+}
+
+void removeHeap(vector<int> &maxHeap) {
+    int n       = maxHeap.size();
+    if(n == 0) return;
+
+    int temp    = maxHeap[n - 1];
+    maxHeap[0]  = temp;
+    maxHeap.pop_back();
+
+    int idx     = 0;
+    int left    = (idx * 2) + 1;
+    int right   = (idx * 2) + 2;
+
+    while(left < n) {
+        int largest = idx;
+
+        if(maxHeap[left] > maxHeap[largest]) 
+            largest = left;
+
+        if(right < n && maxHeap[right] > maxHeap[largest])
+            largest = right;
+
+        if(idx == largest) break;
+        swap(maxHeap[idx], maxHeap[largest]);
+        idx = largest;
+
+        left  = (idx * 2) + 1;
+        right = (idx * 2) + 2; 
+    }
+}
+
+int main() {
+    vector<int> maxHeap;
+    insert(maxHeap, 1);
+    insert(maxHeap, 2);
+    insert(maxHeap, 3);
+    insert(maxHeap, 4);
+    print(maxHeap);
+
+    vector<int> arr = {1, 6, 3, 2 , 9, 4, 7};
+    for(int i = 1; i < arr.size(); i++) {
+        insertInPlace(arr, i);
+    }
+    print(arr);
+    removeHeap(arr);
+    print(arr);
+
+    return 0;
+}
---
+++ Data Structures/Heap/min.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(vector<int> &minHeap) {
+    for (int &a : minHeap) cout << a << " ";
+    cout << endl;
+}
+
+// O(Log N) Heap Insertion Process
+void insert(vector<int> &minHeap, int val) {
+    minHeap.push_back(val);
+    
+    int currIdx   = minHeap.size() - 1;
+    int parentIdx = (currIdx - 1) / 2;
+
+    while(currIdx > 0 && val < minHeap[parentIdx]) {
+        minHeap[currIdx] = minHeap[parentIdx];
+        currIdx          = parentIdx;
+        parentIdx        = (currIdx - 1) / 2; 
+    }
+
+    minHeap[currIdx] = val;
+}
+
+// O(N Log N) Heap Insertion Process
+void insertInPlace(vector<int> &minHeap, int idx) {
+    int temp = minHeap[idx];
+
+    while(idx > 0 && temp < minHeap[(idx - 1) / 2]) {
+        minHeap[idx] = minHeap[(idx - 1) / 2];
+        idx = (idx - 1) / 2;
+    }
+
+    minHeap[idx] = temp;
+}
+
+void removeHeap(vector<int> &minHeap) {
+    int n = minHeap.size();
+    if(n == 0) return;
+    int temp = minHeap[n - 1];
+    minHeap[0] = temp;
+    minHeap.pop_back();
+
+    int idx   = 0;
+    int left  = (idx * 2) + 1;
+    int right = (idx * 2) + 2;
+
+    while(left < minHeap.size()) {
+        int smallest = idx;
+
+        if(minHeap[left] < minHeap[smallest]) 
+            smallest = left; 
+
+        if(right < minHeap.size() && minHeap[right] < minHeap[smallest]) 
+            smallest = right;
+
+        if(smallest == idx) break;
+        swap(minHeap[smallest], minHeap[idx]);
+        idx  = smallest;
+
+        left  = (idx * 2) + 1;
+        right = (idx * 2) + 2; 
+    }
+}
+
+int main() {
+    vector<int> minHeap;
+    insert(minHeap, 10);
+    insert(minHeap, 5);
+    print(minHeap);
+
+    vector<int> arr = {1, 6, 3, 2 , 9, 4, 7};
+    for(int i = 1; i < arr.size(); i++) {
+        insertInPlace(arr, i);
+    }
+    print(arr);
+    removeHeap(arr);
+    print(arr);
+
+    return 0;
+}
