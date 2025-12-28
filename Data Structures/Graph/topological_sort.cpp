1d30d79d7e8f8215b269168cb45b66e1cd1379fd
TantowiPutra <tantowi6758@gmail.com>
2025-12-28 18:42:08 +0700

    Graph: Topological Sort - BFS & DFS

--- Data Structures/Graph/topological_sort.cpp
+++ Data Structures/Graph/topological_sort.cpp
@@ -12,6 +12,55 @@ void dfs(vector<vector<int>> &adj_list, int node, vector<bool> &visited, stack<i
     stk.push(node);
 }
 
+void dfs_topological(vector<vector<int>> adj_list) {
+    vector<bool> visited(adj_list.size(), false);
+    stack<int> stk;
+    
+    for(int i = 0; i < adj_list.size(); i++) {
+        if(!visited[i])
+            dfs(adj_list, i, visited, stk);
+    }
+
+    cout << "Topological Sort (DFS): ";
+    while(!stk.empty()) {
+        cout << stk.top() << " ";
+        stk.pop();
+    }
+
+    cout << endl;
+}
+
+void bfs_topological(vector<vector<int>> adj_list) {
+    vector<int> indegree(adj_list.size());
+
+    for(int i = 0; i < adj_list.size(); i++) {
+        for(int adj : adj_list[i]) {
+            indegree[adj]++;
+        }
+    }
+
+    queue<int> q;
+    for(int i = 0; i < adj_list.size(); i++) {
+        if(indegree[i] == 0)
+            q.push(i);
+    }
+
+    cout << "Topological Sort (BFS): ";
+    while(!q.empty()) {
+        int node = q.front(); q.pop();
+        cout << node << " ";
+
+        for(int adj : adj_list[node]) {
+            indegree[adj]--;
+            if(indegree[adj] == 0)
+                q.push(adj);
+        }
+    }
+    cout << endl;
+
+    return;
+}
+
 int main() {
     vector<vector<int>> adj_list(5);
     adj_list[0] = {1};
@@ -20,21 +69,8 @@ int main() {
     adj_list[3] = {4};
     adj_list[4] = {};
 
-    vector<bool> visited(5, false);
-    stack<int> stk;
-    
-    for(int i = 0; i < 5; i++) {
-        if(!visited[i])
-            dfs(adj_list, i, visited, stk);
-    }
-
-    cout << "Topological Sort: ";
-    while(!stk.empty()) {
-        cout << stk.top() << " ";
-        stk.pop();
-    }
-
-    cout << endl;
+    dfs_topological(adj_list);
+    bfs_topological(adj_list);
 
     return 0;
 }
